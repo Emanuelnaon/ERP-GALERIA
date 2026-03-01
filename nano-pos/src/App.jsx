573cd7d60b6963f896b808472d1f955e110f7972
Emanuel Naon <emanuelnaon@gmail.com>
2026-03-01 01:06:43 -0300

    feat: agregar ruta para la página de inicio en App

--- nano-pos/src/App.jsx
+++ nano-pos/src/App.jsx
@@ -5,8 +5,8 @@ import Dashboard from './pages/Admin/Dashboard';
 
 function App() {
     return (
-        
             <Routes>
+                <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/pos" element={<POS />} />
                 <Route path="/dashboard" element={<Dashboard />} />
